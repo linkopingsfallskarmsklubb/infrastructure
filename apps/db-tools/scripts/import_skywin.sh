1583f8bab9e8f0afc5b68e77ccc491ed750ea16c
masenius <masenius@gmail.com>
2025-09-15 17:18:00 +0200

    rename tables

--- apps/db-tools/scripts/import_skywin.sh
+++ apps/db-tools/scripts/import_skywin.sh
@@ -9,6 +9,7 @@ MYSQL_PORT="${MYSQL_PORT:-3306}"
 MYSQL_USER="${MYSQL_USER:-root}"
 MYSQL_PASSWORD="${MYSQL_PASSWORD}"
 MYSQL_DATABASE="${MYSQL_DATABASE:-skywin}"
+SKYWIN_RENAME_SCRIPT="${SKYWIN_RENAME_SCRIPT:-MySQL.Rename_all_tables.sql}"
 
 # Check required variables
 if [ -z "$GCS_BUCKET" ] || [ -z "$MYSQL_HOST" ] || [ -z "$MYSQL_USER" ] || [ -z "$MYSQL_PASSWORD" ] || [ -z "$MYSQL_DATABASE" ]; then
@@ -24,14 +25,14 @@ if [ -z "$LATEST_FILE" ]; then
 fi
 
 # Download the latest file from Google Cloud Storage
-LOCAL_FILE="import.sql"
-if ! gsutil cp "$LATEST_FILE" "$LOCAL_FILE"; then
+SKYWIN_FILE="skywin.sql"
+if ! gsutil cp "$LATEST_FILE" "$SKYWIN_FILE"; then
   echo "Failed to download file from GCS."
   exit 2
 fi
 
 # Import file into MySQL database
-echo "Importing $LOCAL_FILE into $MYSQL_DATABASE"
+echo "Importing $SKYWIN_FILE into $MYSQL_DATABASE"
 mysql --protocol=tcp \
   --host="$MYSQL_HOST" \
   --user="$MYSQL_USER" \
@@ -39,7 +40,20 @@ mysql --protocol=tcp \
   --port="$MYSQL_PORT" \
   --default-character-set=utf8 \
   --comments \
-  --database="$MYSQL_DATABASE" <"$LOCAL_FILE"
+  --database="$MYSQL_DATABASE" <"$SKYWIN_FILE"
+
+# Rename tables: https://skywin.se/download
+RENAME_FILE=rename.sql
+echo "Importing $RENAME_FILE into $MYSQL_DATABASE"
+curl https://www.skywinner.se/skywin/_files/sql_ddl/MySQL.Rename_all_tables.sql >$RENAME_FILE
+mysql --protocol=tcp \
+  --host="$MYSQL_HOST" \
+  --user="$MYSQL_USER" \
+  --password="$MYSQL_PASSWORD" \
+  --port="$MYSQL_PORT" \
+  --default-character-set=utf8 \
+  --comments \
+  --database="$MYSQL_DATABASE" <"$RENAME_FILE"
 
 if [ $? -ne 0 ]; then
   echo "Failed to import file into MySQL."
